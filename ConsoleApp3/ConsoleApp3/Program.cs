4b06396984be852c1626e82337533eaab08bb9a8
imcutedora <145494538+imcutedora@users.noreply.github.com>
2023-09-19 23:08:14 +0300

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace powerofthree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if (Math.Log(num, 3) % 1 == 0 || Math.Log(num, 3) % 1 >= 0.9999999999)
+            {
+                Console.WriteLine("Это степень числа 3.");
+            }
+            else
+            {
+                Console.WriteLine("Это не степень числа 3.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
