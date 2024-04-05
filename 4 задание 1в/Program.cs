38b385491970b0c0edae4d3a890451502e259187
imcutedora <145494538+imcutedora@users.noreply.github.com>
2024-04-05 11:37:06 +0300

    05.04.24

---
+++ 4 задание 1в/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            var dict = new Dictionary<string, double>();
+
+            dict.Add("молоко", 89.99);
+            dict.Add("хлеб", 49.99);
+
+            foreach (var e in dict.Keys)
+            {
+                dict[e] = Math.Round(dict[e] * 1.1, 2);
+            }
+
+            foreach (var e in dict.Values)
+                Console.WriteLine(e);
+        }
+    }
+}
---
+++ 5 задание 1в/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] { -2, 56, 14, 32, -99, 374 };
+
+            Console.WriteLine(GetAverage<int>(nums));
+            Console.WriteLine(nums.Average() + " - для сравнения");
+        }
+
+        public static double GetAverage<T>(T[] elems)
+        {
+            double sum = default;
+
+            foreach (var e in elems)
+                sum += Convert.ToDouble(e);
+
+            return sum / elems.Length;
+        }
+    }
+}
